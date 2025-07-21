22f3f2b0c15665d2f1d2a992aae21e15125704f4
minhtan <ttannn1104@gmail.com>
2025-07-22 00:17:59 +0700

    form

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -2,9 +2,39 @@ import React from "react";
 
 
 class MyComponent extends React.Component {
+    state ={
+        name : 'user',
+        address : "quan nhan",
+        age : 21 ,
+           };
+
+        handleonChangeInput = (event) => {
+            this.setState({
+                name : event.target.value
+            })
+        }
+
+        handleClick = (event) => {
+            event.preventDefault()
+            console.log(this.state)
+        }
+
+
+
+           
+//JSX
     render () {
         return (
-            <div>My div coponents</div>
+            <div>
+               toi la nguoi su dung :{this.state.name} anf i 'm from {this.state.address}
+               <form onSubmit={(event) => this.handleClick(event)}>
+                <input onChange={(event) => this.handleonChangeInput(event) }/>
+                <button>click me </button>
+
+
+               </form>
+
+            </div>
         );
     }
 }
